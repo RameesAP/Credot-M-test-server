d2019e28fbc6da6cad90bc9ebe91892991212939
RameesAP <rameeswork@gmail.com>
2024-02-14 16:54:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+dotenv.config();
+
+
+const app =express();
