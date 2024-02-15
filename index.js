1036d5622e75a9ba11b8235a5a537b3ab2d05400
RameesAP <rameeswork@gmail.com>
2024-02-16 01:08:50 +0530

    working progress

--- index.js
+++ index.js
@@ -1,9 +1,44 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import dotenv from 'dotenv'
-import morgan from 'morgan'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(cookieParser());
 
-const app =express();
+// import routers
+import userRoutes from "./routes/user_routes.js";
+// import authRoutes from "./routes/auth_routes.js";
+
+// usee of routes
+app.use("/api/user", userRoutes);
+// app.use("/api/auth", authRoutes);
+
+//middleware
+// app.use((error, req, res, next) => {
+//   statusCode = error.statusCode || 500;
+//   const message = error.message || "internal server Error";
+//   return res.status(statusCode).json({
+//     success: false,
+//     statusCode,
+//     message,
+//   });
+// });
+
+// DB Connect
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to Mongo DB !!!");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running on port 5000");
+});
