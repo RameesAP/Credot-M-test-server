92af18c78d592c19f0924902dd1e21989a300b6e
RameesAP <rameeswork@gmail.com>
2024-02-19 00:27:29 +0530

    u

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use("/api/cart", cartRoutes);
 
 // middleware
 app.use(cors({
-  origin: "https://credotrameess.netlify.app/",
+  origin: "https://credotramees.netlify.app/",
   credentials: true,
 }));
 
